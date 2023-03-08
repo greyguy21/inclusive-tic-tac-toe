057611616c330b026cd252f200196e7bb03387e8
greyguy21 <e0559779@u.nus.edu>
2023-03-09 01:47:02 +0800

    Add Welcome Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-import io from "socket.io-client"; 
+import "./App.css"
+import WelcomePage from "./pages/WelcomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const socket = io('http://localhost:4000/');
-
-function App() {
-  socket.emit("hello from client", "hello from client");
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return ( 
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<WelcomePage/>}/>
+    </Routes>
+  </BrowserRouter>
+  )
 }
+// import logo from './logo.svg';
+// import './App.css';
+// import io from "socket.io-client"; 
+
+// const socket = io('http://localhost:4000/');
+
+// function App() {
+//   socket.emit("hello from client", "hello from client");
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ client/src/pages/WelcomePage.js
@@ -0,0 +1,12 @@
+import "../App.css"
+
+const WelcomePage = () => {
+  return ( 
+    <div className="App">
+      <h1>Welcome to Tic Tac Toe</h1>
+      <button>Start</button>
+    </div>
+  )
+}
+
+export default WelcomePage;
